d343f00c4622dd3387e989d5d8fea28434b2f773
Eric Quintal <canulquintalerik@gmail.com>
2022-02-18 14:37:22 -0600

    prueba de commit

--- EricQuintal/VentanaConLayouts.java
+++ EricQuintal/VentanaConLayouts.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+//hola
 
 public class VentanaConLayouts extends Frame implements ActionListener {
 	//Widegs
