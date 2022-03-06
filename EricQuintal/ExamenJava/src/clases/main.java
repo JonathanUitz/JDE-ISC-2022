2d6e3a5850669f1e2a6a11b2d4b313eab09cbf99
EricCQuintal <7302@itescam.edu.mx>
2022-03-05 19:52:37 -0600

    Prueba proyecto

---
+++ EricQuintal/ExamenJava/src/clases/main.java
@@ -0,0 +1,15 @@
+
+package EricQuintal.ExamenJava.src.clases;
+
+import EricQuintal.ExamenJava.src.ventanas.Examen;
+
+public class main {
+
+   
+    public static void main(String[] args) {
+       
+        Examen e = new Examen();
+        e.setVisible(true);
+        
+    }
+}
