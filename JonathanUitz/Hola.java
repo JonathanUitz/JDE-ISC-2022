1fa94669696c936548b3778550a1af8adc330a4c
JonathanUitz <80001016+JonathanUitz@users.noreply.github.com>
2022-03-15 17:48:55 -0600

    Update Hola.java

--- JonathanUitz/Hola.java
+++ JonathanUitz/Hola.java
@@ -3,7 +3,9 @@ package JonathanUitz;
 public class Hola {
     
     public static void main(String[] args) {
+        System.out.println("");
         System.out.println("Hola estoy programando");    
+        System.out.println("");
     }
 
 }
