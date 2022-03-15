63b7baf613be47a170cf5aca5c167a0c5326c6b2
JonathanUitz <80001016+JonathanUitz@users.noreply.github.com>
2022-03-15 17:48:51 -0600

    Create Mundo.java

---
+++ JonathanUitz/Ejercicio4/Mundo.java
@@ -0,0 +1,30 @@
+package JonathanUitz.Ejercicio4;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Mundo extends Canvas{
+
+    int x, y;
+    int w, h;
+
+    public Mundo(){
+        x=20;
+        y=28;
+        w = h = 100;
+    }
+
+    public void paint(Graphics g){
+
+        for(int x=0; x<100;x++){
+
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, w, h);
+        g.fillOval(x, y, w, h);
+        g.setColor(Color.WHITE);
+        g.drawString("Hola mundo", 28, 74);
+
+        }
+    }
+    
+}
