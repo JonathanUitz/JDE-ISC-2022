6972c862744e3879ff87a9b32ffeb01be4829162
JonathanUitz <80001016+JonathanUitz@users.noreply.github.com>
2022-03-15 18:28:01 -0600

    Update Main.java

--- JonathanUitz/Ejercicio4/Main.java
+++ JonathanUitz/Ejercicio4/Main.java
@@ -4,27 +4,76 @@ import java.awt.Panel;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Main extends Frame{
+public class Main extends Frame implements MouseListener, MouseMotionListener{
 
     Panel p;
+    Mundo mundo;
 
     public Main(){
 
         p = new Panel();
         this.setLayout(new BorderLayout());
         p.setLayout(new BorderLayout());
-        p.add(new Mundo());
+        mundo = new Mundo();
+        p.add(mundo);
+        mundo.addMouseListener(this);
+        mundo.addMouseMotionListener(this);
         this.add(p, BorderLayout.CENTER);
         this.setSize(500, 400);
         this.setVisible(true);
+        this.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
 
     }
 
     public static void main(String[] args) {
-        new Main().addWindowListener(new WindowAdapter(){
-            public void windowClosing(WindowEvent e){
-                System.exit(0);
-            }
-        });   
+        Main canvasI = new Main();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Dragged");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Moved");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Clicked");
+        mundo.setX(e.getX());
+        mundo.setY(e.getY());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Pressed");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Released");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Entered");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("Exited");
     }
 }
