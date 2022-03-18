02297bab7ec4b96107853588e4f899cb4c49dc39
Diego Chi <80001208+V0LTERS@users.noreply.github.com>
2022-03-18 15:35:33 -0600

    Update Ejercicio3.java

--- DiegoChi/Lira/Ejercicio3.java
+++ DiegoChi/Lira/Ejercicio3.java
@@ -3,11 +3,17 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
+
 public class Ejercicio3 extends Canvas {
 
     private int x2, y2, w2, h2;
     int x, y;
     int w, h; 
+
+    int food_x;
+    int food_y;
+    int food_s;
+
     public Ejercicio3(){
         x = 28;
         y = 50;
@@ -17,6 +23,7 @@ public class Ejercicio3 extends Canvas {
     public void paint (Graphics g){
 
             g.setColor(Color.BLACK);
+            colision();
             g.drawRect(x, y, w, h);
             g.fillOval(x, y, w, h);
             g.setColor(Color.WHITE);
@@ -24,7 +31,15 @@ public class Ejercicio3 extends Canvas {
 
             g.setColor(Color.BLUE);
             g.fillRect(x2,y2,20,20);
+    }
 
+    public void colision(){
+        if(x >= food_x && y >= food_y &&
+        (x + w) >food_x && (y + h)> food_y
+        ){
+            food_x = (int)(Math.random()*500);
+            food_y = (int)(Math.random()*400);
+        }
     }
     public void numGe() {
         int min = 60;
@@ -34,6 +49,9 @@ public class Ejercicio3 extends Canvas {
        y2 = random.nextInt(max + min) + min;
     }
     
+    public int getX(){
+        return x;
+    }
     public void setX(int x) {
         this.x = x;
     }
