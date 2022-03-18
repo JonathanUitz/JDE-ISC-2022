b8ef7eedfe242c8f2f039b9426e09a368781bc36
JonathanUitz <80001016+JonathanUitz@users.noreply.github.com>
2022-03-18 15:51:48 -0600

    Update Mundo.java

--- JonathanUitz/Ejercicio4/Mundo.java
+++ JonathanUitz/Ejercicio4/Mundo.java
@@ -8,20 +8,49 @@ public class Mundo extends Canvas{
     int x, y;
     int w, h;
 
+    int food_x;
+    int food_y;
+    int food_s;
+
     public Mundo(){
         x=20;
         y=28;
         w = h = 100;
+
+        food_x = (int)(Math.random()*500);
+        food_y = (int)(Math.random()*400);
+        food_s = 20;
     }
 
     public void paint(Graphics g){
 
+        colision();
+
+        g.setColor(Color.YELLOW);
+        g.fillOval(food_x, food_y, food_s, food_s);
         g.setColor(Color.BLACK);
         g.drawRect(x, y, w, h);
         g.fillOval(x, y, w, h);
         g.setColor(Color.WHITE);
         g.drawString("Hola mundo", x, (y+h/2));
 
+    }
+
+    public void colision(){
+        /**
+         * La posicion "x" de la principal es mayor que la posicion de la secundaria
+         * La posicion "x" de la principal es menor que la posicion de la secundria mas 
+         * La posicion de "y" de la principal es mayor que la posicion de y de la secuncaria
+         * La posicion de "y" de la principal es menor que la posicion de la secundaria mas 
+         */
+        if(food_x > x && 
+           food_x < (x+w) &&
+           food_y > y &&
+           food_y < (y+h)
+        ){
+                food_x = (int)(Math.random()*500);
+                food_y = (int)(Math.random()*400);
+        }
     }
 
     public int getX(){
