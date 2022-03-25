c7663ec557f395ccc6ca03f90d1318425a7fb016
JonathanUitz <80001016+JonathanUitz@users.noreply.github.com>
2022-03-25 05:36:32 -0600

    Update ventana.java

--- JonathanUitz/Ejercicio1/ventana.java
+++ JonathanUitz/Ejercicio1/ventana.java
@@ -1,6 +1,11 @@
 
-
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
