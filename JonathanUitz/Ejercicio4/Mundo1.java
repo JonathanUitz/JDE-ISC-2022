dd800386c8e38c8a23b75d32d4bedc3aba0b5045
Diego Chi <80001208+V0LTERS@users.noreply.github.com>
2022-03-24 20:35:54 -0600

    Create Mundo1.java

---
+++ JonathanUitz/Ejercicio4/Mundo1.java
@@ -0,0 +1,29 @@
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Mundo1 extends Canvas{
+
+    int x, y;
+    int w, h;
+
+    public Mundo1(){
+        x=20;
+        y=28;
+        w = h = 100;
+    }
+
+    public void paint(Graphics g){
+
+
+            g.setColor(Color.BLACK);
+            g.drawRect(x, y, w, h);
+            g.fillOval(x, y, w, h);
+            g.setColor(Color.WHITE);
+            g.drawString("Hola mundo", x, (y+h/2));
+
+        
+    }
+    
+}
