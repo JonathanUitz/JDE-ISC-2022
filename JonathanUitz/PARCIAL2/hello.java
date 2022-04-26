6131311fe497933c40e6626e2248edc53a281c6d
JonathanUitz <80001016+JonathanUitz@users.noreply.github.com>
2022-04-26 17:36:57 -0500

    Create hello.java

---
+++ JonathanUitz/PARCIAL2/hello.java
@@ -0,0 +1,11 @@
+package JonathanUitz;
+
+public class hello {
+    
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Hola estoy programando mi segundo parcial");    
+        System.out.println("");
+    }
+
+}
